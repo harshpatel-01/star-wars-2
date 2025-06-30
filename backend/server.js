de67ead655d506d329cfb0537a3ee189a6addfd5
Harsh Patel <patelharshharsh1@gmail.com>
2025-06-30 16:08:53 -0400

    fix: updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
-import express from 'express'
-import cors from 'cors'
-import connectToDB from './config/connectToDB.js'
+import express from 'express';
+import cors from 'cors';
+import connectToDB from './config/connectToDB.js'; 
+import characterRoutes from './routes/charactersRoutes.js'; 
 
-const PORT = process.env.PORT
-const app = express()
+const PORT = process.env.PORT || 5000; 
+const app = express();
 
-const { db } = await connectToDB()
+app.use(cors()); 
+app.use(express.json());  
 
+const { db } = await connectToDB();
 
-app.get("/", async (req,res) => {
+app.get("/", (req, res) => {
+    res.send("Welcome to the Star Wars API!");
+});
 
-    const characters =  db.collection('characters')
-    const name = await characters.find({}).toArray()
-
-    res.json(name);
-})
-
+app.use('/api/characters', characterRoutes);  
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
