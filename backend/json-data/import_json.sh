2993ab6ce428ba327422178cfb719426aa53f9cf
Harsh Patel <patelharshharsh1@gmail.com>
2025-06-30 11:22:00 -0400

    feat: imported data into mongo

---
+++ backend/json-data/import_json.sh
@@ -0,0 +1,6 @@
+for file in *.json; do
+    collection_name=$(basename "$file" .json) 
+    mongoimport --uri="mongodb://localhost:27017" --db sw-db --collection "$collection_name" --file "$file" --jsonArray
+done
+
+
