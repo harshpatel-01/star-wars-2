78ef0d0038f8f6934e218e3cf9715809378fe1c3
Harsh Patel <patelharshharsh1@gmail.com>
2025-06-30 12:14:10 -0400

    added comments

--- backend/json-data/import_json.sh
+++ backend/json-data/import_json.sh
@@ -1,5 +1,6 @@
-for file in *.json; do
-    collection_name=$(basename "$file" .json) 
+# Uses a loop to import the data into the DB
+for file in *.json; do # matches all files with .json in the current directory
+    collection_name=$(basename "$file" .json) # extracts the base name of the file
     mongoimport --uri="mongodb://localhost:27017" --db sw-db --collection "$collection_name" --file "$file" --jsonArray
 done
 
