3af3a00d059bf5e5456c42a3323bef9a2d277de5
Harsh Patel <patelharshharsh1@gmail.com>
2025-06-30 16:37:42 -0400

    feat: added components dir

--- front-end/star-wars-2/src/App.jsx
+++ front-end/star-wars-2/src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CharactersList from './components/CharactersList';
 
-function App() {
+function App() {z
   const [count, setCount] = useState(0)
 
   return (
